f6c6df1d1002052e05f7e06bfc553564859b3c54
arsheenkour <arsheenkour47@gmail.com>
2024-06-06 10:50:00 +0530

    first commit

---
+++ lecture4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //declare 2 variables and then assign the values of 3, 5 and then add them
+    int a,b;
+    a=3;
+    b=1;
+    int c;
+    c=a+b;
+    cout<<a-b<<endl;
+    cout<<a+b;
+}
